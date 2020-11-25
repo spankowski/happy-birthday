ac934a1abff328064e95f2a259b6c6fb133d9fb9
Szymon Pankowski <57172847+spankowski@users.noreply.github.com>
2020-11-25 08:23:29 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -195,8 +195,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
+      x: 100,
+      y: 450,
       rotation: -180,
       opacity: 0
     })
